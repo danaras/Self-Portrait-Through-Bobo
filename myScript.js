97d2ecf22a442da3a68a9b3f856d5c046449943a
Danara Sarioglu <ds4489@nyu.edu>
2015-09-15 00:32:38 -0400

    First commit

---
+++ myScript.js
@@ -0,0 +1,57 @@
+function init() {
+
+var c = document.getElementById("myCanvas");
+var context = c.getContext("2d");
+context.fillStyle = "white";
+context.beginPath();
+context.arc(95, 85, 40, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+context.lineWidth = 10;
+context.stroke();
+context.fillStyle = "black";
+
+document.getElementById("myCanvas").addEventListener("mouseover",function(){
+// Draw the left eye
+context.beginPath();
+context.arc(75, 75, 5, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+
+// Draw the right eye
+context.beginPath();
+context.arc(114, 75, 5, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+
+// Draw the mouth
+context.beginPath();
+context.arc(95, 90, 26, Math.PI, 2*Math.PI, true);
+context.closePath();
+context.fill();
+});
+document.getElementById("myCanvas").addEventListener("mouseout",function(){
+	context.fillStyle = "white";
+context.beginPath();
+context.arc(95, 85, 40, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+context.lineWidth = 10;
+context.stroke();
+context.fillStyle = "black";
+});
+}
+window.addEventListener('load', init);
+
+
+
+
+	for(var i=1; i<4; i++){
+		document.getElementById("i").loop=true;
+	}
+	// if(document.getElementById("1").paused){
+
+	// }
+
+	
+
